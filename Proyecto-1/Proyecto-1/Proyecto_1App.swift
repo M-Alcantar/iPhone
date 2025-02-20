7d4e5f3b5b9014da9d913be876a43086a71662f5
M-Alcantar <129239276+M-Alcantar@users.noreply.github.com>
2025-02-19 18:44:02 -0800

    Crear Proyecto 1
    Yeah

---
+++ Proyecto-1/Proyecto-1/Proyecto_1App.swift
@@ -0,0 +1,17 @@
+//
+//  Proyecto_1App.swift
+//  Proyecto-1
+//
+//  Created by CETYS Universidad  on 19/02/25.
+//
+
+import SwiftUI
+
+@main
+struct Proyecto_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Proyecto-1/Proyecto-1Tests/Proyecto_1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  Proyecto_1Tests.swift
+//  Proyecto-1Tests
+//
+//  Created by CETYS Universidad  on 19/02/25.
+//
+
+import Testing
+@testable import Proyecto_1
+
+struct Proyecto_1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
