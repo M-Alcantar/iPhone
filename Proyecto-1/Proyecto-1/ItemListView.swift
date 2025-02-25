4184cad9e1ac753c3279124e4e5c712b00ce9e35
M-Alcantar <129239276+M-Alcantar@users.noreply.github.com>
2025-02-24 21:41:48 -0800

    help me god 1

--- Proyecto-1/Proyecto-1/ItemListView.swift
+++ Proyecto-1/Proyecto-1/ItemListView.swift
@@ -1,53 +1,59 @@
 //
-//  ItemListView.swift
+//  ItemView.swift
 //  Proyecto-1
 //
-//  Created by CETYS Universidad  on 24/02/25.
+//  Created by CETYS Universidad  on 19/02/25.
 //
 
 import SwiftUI
-struct ItemListView: View {
-    @State private var items: [Item] = [
-    Item(name: "Saxophong",
-         description: "Ir a Wallmart", price:"599.99"),
-    Item(name: "Leer tutorial de SwiftUI",
-         description: "Entrar al código y video en Youtube", price:"80")
-    ]
-    
-    @State private var showAddItemView:Bool = false
+
+struct ItemView: View {
+    @State var item : Item
     
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink(destination: ItemView(item: item)) {
+        let screenWidth = UIScreen.main.bounds.width
+        
+        ScrollView {
+            VStack(spacing: 20) {
+                HStack(){
+                    VStack(alignment: .leading){
                         Text(item.name)
+                            .font(.largeTitle)
+                            .fontWeight(.bold)
+                            .foregroundColor(.white)
+                            .multilineTextAlignment(.leading)
+                            .padding()
+                            .frame(width: screenWidth * 0.63, alignment: .leading)
+                    }
+                    
+                    VStack(alignment: .center){
+                        Text("$" + item.price)
+                            .font(.title)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                            .padding(5)
+                            .background(Color.green)
+                            .cornerRadius(5)
+                            .frame(alignment: .trailing)
                     }
                 }
-                .onDelete(perform: deleteItem)
-            }
-            .navigationTitle("Lista de productos")
-            .toolbar{
-                ToolbarItem(placement: .navigationBarTrailing){
-                    Button(action: {
-                        showAddItemView = true
-                    }) {
-                        Image(systemName: "plus")
-                    }
+                    .frame(maxWidth: .infinity)
+                    .background(LinearGradient(colors: [Color.green, Color.pink, Color.brown], startPoint: .leading, endPoint: .trailing))
+                    .shadow(radius: 3)
+                
+                VStack(alignment: .center, spacing: 10){
+                    Text(item.price)
+                        .font(.title2)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.gray)
                 }
-                ToolbarItem(placement: .navigationBarLeading){
-                    EditButton()
-                }
-            }
-            .sheet(isPresented: $showAddItemView) {
-                AddItemView(items: $items)
             }
         }
     }
+}
     
-    func deleteItem(at offset: IndexSet) {
-        items.remove(atOffsets: offset)
-    }
+#Preview {
+    ItemView(item: Item(name: "Nombre del prodoto", description: "Descripcion del producto", price: "500.49", image: UIImage(systemName: "photo")))
 }
 
 #Preview {
